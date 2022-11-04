087e1d8f19d12a789fec7f0477a385272f8a65ca
tony tolver <andrademaresantonio00@gmail.com>
2022-11-03 21:40:57 -0600

    Proteccion de ruta posts.index por medio de middleware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    //autenticacion
+    //muestra antes de mostrar el index que el usuario este autenticado
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('dashboard');
