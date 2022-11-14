4d9a7d69fb471cab8be27954404ac0fee76eabcc
tony tolver <andrademaresantonio00@gmail.com>
2022-11-13 18:36:41 -0600

    Almacenar imagen en el servidor

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
@@ -11,8 +13,23 @@ class ImagenController extends Controller
     {
         $imagen = $request->file('file');
 
-        //validar que el archivo sea una imagen
-        return response()->json(['imagen' => 'probando respuesta']);
-    }
+        // generar un id unico para cada una de las imagenes
+        $nombreImagen = Str::uuid() . '.' . $imagen->extension();
+        
+        // imagen guardada en el servidor
+        $imagenServidor = Image::make($imagen);
+        //cada imagen va a medir 1000 x 1000px
+        $imagenServidor->fit(1000, 1000); //height y width
 
+        // guardar la imagen en el servidor
+        $imagenPath = public_path('uploads' . '/' . $nombreImagen);
+        $imagenServidor->save($imagenPath); //guardar la imagen en el servidor
+
+        //validar que el archivo sea una imagen
+        return response()->json(['imagen' => $nombreImagen]);
+    }
 }
+
+
+// cuando se sube una imagen se va a uploads y se guarda con un nombre unico
+// solo se guarda el ui de la imagen en la base de datos, no la imagen en si
